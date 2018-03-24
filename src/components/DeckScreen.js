a4369f2f0ce6607bd09be23b92848f1d86002e58
kxkannan <kxkannan@gmail.com>
2018-03-23 19:10:00 -0600

    added deck screen

---
+++ src/components/DeckScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+
+class DeckScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Deck View</Text>
+            </View>
+        )
+    }
+}
+
+export default DeckScreen
